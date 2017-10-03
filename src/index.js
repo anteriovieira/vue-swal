635065d9b742544dfc00add6a738525e29c1e6fe
Antério vieira <anteriovieira@gmail.com>
2017-10-03 20:43:04 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import swal from 'sweetalert'
+
+function plugin (Vue) {
+  Vue.prototype.$swal = swal
+}
+
+// Install by default if using the script tag
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(plugin)
+}
+
+export default plugin
+const version = '__VERSION__'
+// Export all components too
+export {
+  version
+}
